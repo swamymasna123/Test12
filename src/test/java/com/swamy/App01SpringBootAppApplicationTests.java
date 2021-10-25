f9c07e175886248902fd15e6d0b260bcb9459bc3
swamymasna <swamymasna55@gmail.com>
2021-10-25 13:03:01 +0530

    First Commit

---
+++ src/test/java/com/swamy/App01SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swamy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App01SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
