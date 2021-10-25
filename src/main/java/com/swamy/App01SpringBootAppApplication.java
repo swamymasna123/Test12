3b8a5155c2855767a7fda873dec689d70410630d
swamymasna <swamymasna55@gmail.com>
2021-10-25 13:08:55 +0530

    First commit

--- src/main/java/com/swamy/App01SpringBootAppApplication.java
+++ src/main/java/com/swamy/App01SpringBootAppApplication.java
@@ -2,12 +2,19 @@ package com.swamy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class App01SpringBootAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(App01SpringBootAppApplication.class, args);
+		
+//		Added Some Logic inside the Starter class
+		ConfigurableApplicationContext context = null;
+		context = SpringApplication.run(App01SpringBootAppApplication.class, args);
+		System.out.println("From Starter class...!");
+		context.close();
 	}
 
 }
